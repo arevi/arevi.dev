76c41e9d208fc3ba186cacdc0a6891d87a2ccf28
Arevi <Arevi.Dev@gmail.com>
2019-12-05 01:00:06 -0500

    Added click event to h1

--- client/src/Components/Greeting/Greeting.jsx
+++ client/src/Components/Greeting/Greeting.jsx
@@ -5,10 +5,18 @@ import './greeting.css';
 function Greeting() {
   const [name, setName] = useState('Armin Dizdarevic');
 
+  function changeName() {
+    name === 'Armin Dizdarevic'
+      ? setName('Arevi')
+      : setName('Armin Dizdarevic');
+  }
+
   return (
-    <div id="greeting">
-      <h1 id="greeting-text">Hello, my name is</h1>
-      <h1 id="greeting-name">{name}</h1>
+    <div id='greeting'>
+      <h1 id='greeting-text'>Hello, my name is</h1>
+      <h1 id='greeting-name' onClick={changeName}>
+        {name}
+      </h1>
     </div>
   );
 }
